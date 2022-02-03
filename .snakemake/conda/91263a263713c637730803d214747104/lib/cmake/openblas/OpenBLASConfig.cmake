2e94231ffb2757fc9530ad9acc57db79b739a80f
Jaehyun Kim <a7420174@gmail.com>
2022-02-03 10:26:05 +0000

    Update

--- .snakemake/conda/91263a263713c637730803d214747104/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.3.12")
-SET(OpenBLAS_INCLUDE_DIRS /workspace/ABC-Enhancer-Gene-Prediction/.snakemake/conda/91263a263713c637730803d214747104/include)
-SET(OpenBLAS_LIBRARIES /workspace/ABC-Enhancer-Gene-Prediction/.snakemake/conda/91263a263713c637730803d214747104/lib/libopenblas.so)
